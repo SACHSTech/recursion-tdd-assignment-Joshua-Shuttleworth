51c4f8b174a24043289a7d68dacc9e50c375f3ad
Joshua Shuttleworth <joshua.shuttleworth22@ycdsbk12.ca>
2021-05-20 19:57:03 -0400

    TDD2

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -15,12 +15,16 @@ public class RecursionTest{
   public void Test1(){
     assertEquals(2, Recursion.count7(717));
   }
+  @Test
+  public void Test2() {
+    assertEquals(1, Recursion.count7(7));
+  }
     
   // add more tests
 }
 
 /*
-* javac -d bin src/recursionAssignment/RecursionTest.java
+* javac -d bin src/recursionAssignment/Recursion.java
 * 
 * javac -d bin -sourcepath src -classpath lib/junit-platform-console-standalone-1.7.0-M1.jar src/recursionAssignment/RecursionTest.java 
 *
