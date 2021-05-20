e1fa70bd3d699ffe7ccff5d9d531f206fb568fe2
Joshua Shuttleworth <joshua.shuttleworth22@ycdsbk12.ca>
2021-05-20 17:03:36 -0400

    TDD1

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -1,5 +1,11 @@
 package recursionAssignment;
 
-public class Recursion{
-
+public class Recursion {
+	public static int count7(int n) {
+		if(n%10 == 7)
+			return 1+count7(n/10);
+		if(n%10 != n)
+			return count7(n/10);
+		return 0;
+	}
 }
--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,34 +1,28 @@
 package recursionAssignment;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Before;
-import org.junit.jupiter.api.After;
+//import org.junit.jupiter.api.Before;
+//import org.junit.jupiter.api.After;
 
 import recursionAssignment.Recursion;
 
 public class RecursionTest{
     
-    @Before
-    public void beforeTest(){
-      // some steps to perform before tests are run
-    }
-    
     /**
      * An initial test
      */
-    @Test
-    public void Test1(){
-        // make assertion statement(s)
-        // assertEquals(EXPECTEDVALUE, TESTVALUE);
-        // assertTrue(TESTBOOLEAN);
-    }
+  @Test
+  public void Test1(){
+    assertEquals(2, Recursion.count7(717));
+  }
     
-    // add more tests
-    
-    @After
-    public void afterTest(){
-      // some steps to perform after tests are run
-    }
-    
-    
+  // add more tests
 }
+
+/*
+* javac -d bin src/recursionAssignment/RecursionTest.java
+* 
+* javac -d bin -sourcepath src -classpath lib/junit-platform-console-standalone-1.7.0-M1.jar src/recursionAssignment/RecursionTest.java 
+*
+* java -jar lib/junit-platform-console-standalone-1.7.0-M1.jar --class-path bin -c recursionAssignment.RecursionTest
+*/
