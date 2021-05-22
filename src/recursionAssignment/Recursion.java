de923a088f54c0c7454e9244b4496e01e2ecd68a
Joshua Shuttleworth <joshua.shuttleworth22@ycdsbk12.ca>
2021-05-22 11:20:36 -0400

    Added comments (A bit past the due date but everything else was done before I just did not know if you wanted them or not)

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -1,25 +1,50 @@
 package recursionAssignment;
 
 public class Recursion {
+
+    /**
+    *	Counts how many times the digit 7 is present in the given number.
+    * 	@author: Joshua. S 
+    */
 	public static int count7(int n) {
+		// Recursive Case
 		if(n%10 == 7)
-			return 1+count7(n/10);
+			return 1 + count7(n / 10);
 		if(n%10 != n)
-			return count7(n/10);
+			return count7(n / 10);
+
+		// Base Case
 		return 0;
 	}
+
+	/**
+    *	Change all instances of the string "pi" to "3.14".
+    * 	@author: Joshua. S 
+    */
 	public static String changePi(String str) {
+		// Recursive Case
 		if(str.length() > 1 && (str.substring(0, 2)).equals("pi"))
-			return "3.14"+changePi(str.substring(2));
+			return "3.14" + changePi(str.substring(2));
 		if(str.length() != 0)
-			return str.substring(0, 1)+changePi(str.substring(1));
+			return str.substring(0, 1) + changePi(str.substring(1));
+
+		// Base Case
 		return "";
 	}
+
+	/**
+    *	Retern true if a number in the array is followed by it's
+	*	self times 10. If that never ocurse return false.
+    * 	@author: Joshua. S 
+    */
 	public static boolean array220(int[] intNums, int index) {
-		if(index == intNums.length-1)
+		// Base Case
+		if(index == intNums.length - 1)
 			return false;
-		if((intNums[index]*10) == intNums[index+1])
+
+		// Recursive Case
+		if((intNums[index] * 10) == intNums[index + 1])
 			return true;
-		return array220(intNums, index+1);	
+		return array220(intNums, index + 1);	
 	}
 }
--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,18 +1,13 @@
 package recursionAssignment;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.Before;
-//import org.junit.jupiter.api.After;
-
 import recursionAssignment.Recursion;
 
-public class RecursionTest{
-    
-    /**
-     * An initial test
-     */
+public class RecursionTest {
+
+  // Tests for count7
   @Test
-  public void count7Test1(){
+  public void count7Test1() {
     assertEquals(2, Recursion.count7(717));
   }
   @Test
@@ -24,6 +19,7 @@ public class RecursionTest{
     assertEquals(0, Recursion.count7(123));
   }
 
+  // Tests for changePi
   @Test
   public void changePiTest1() {
     assertEquals("x3.14x", Recursion.changePi("xpix"));
@@ -40,35 +36,36 @@ public class RecursionTest{
   public void changePiTest4() {
     assertEquals("iiipp3.143.143.14iip3.14ii", Recursion.changePi("iiipppipipiiippiii"));
   }
-    
+  
+  // Tests for array220
   @Test
-  public void array220Test1(){
+  public void array220Test1() {
     int[] arr = {1, 2, 20};
     assertEquals(true, Recursion.array220(arr, 0));
   }
   @Test
-  public void array220Test2(){
+  public void array220Test2() {
     int[] arr = {3, 30};
     assertEquals(true, Recursion.array220(arr, 0));
   }
   @Test
-  public void array220Test3(){
+  public void array220Test3() {
     int[] arr = {3};
     assertEquals(false, Recursion.array220(arr, 0));
   }
   @Test
-  public void array220Test4(){
+  public void array220Test4() {
     int[] arr = {1, 2, 24, 38, 5, 67};
     assertEquals(false, Recursion.array220(arr, 0));
   }
   @Test
-  public void array220Test5(){
+  public void array220Test5() {
     int[] arr = {8, 4, 63, 87, 9, 90};
     assertEquals(true, Recursion.array220(arr, 0));
   }
-  // add more tests
 }
 
+// Compile commands
 /*
 * javac -d bin src/recursionAssignment/Recursion.java
 * 
